418256e3b8a56391308e9339753b5b1f8343129a
Prashant <prashantco111@gmail.com>
2025-08-24 23:48:40 +0530

    CHORE: Added minor doc

--- envcfg/src/lib.rs
+++ envcfg/src/lib.rs
@@ -3,6 +3,7 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::{DeriveInput, parse_macro_input};
 
+/// ⚠️WARNING: WIP
 #[proc_macro_derive(EnvCfg, attributes(env_cfg))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(input as DeriveInput);
